5fd1b4098271aff4cd72371180767a1d9df969ba
gabrielmdss <gersongabriel_@hotmail.com>
2025-08-05 00:19:11 -0300

    feat: motivos component

--- src/components/content/content.component.ts
+++ src/components/content/content.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { InicioComponent } from "../inicio/inicio.component";
 import { ServicosComponent } from "../servicos/servicos.component";
 import { PagamentoComponent } from "../pagamento/pagamento.component";
+import { MotivosComponent } from "../motivos/motivos.component";
 
 @Component({
   selector: 'app-content',
-  imports: [InicioComponent, ServicosComponent, PagamentoComponent],
+  imports: [InicioComponent, ServicosComponent, PagamentoComponent, MotivosComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
---
+++ src/components/motivos/motivos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-motivos',
+  imports: [],
+  templateUrl: './motivos.component.html',
+  styleUrl: './motivos.component.css'
+})
+export class MotivosComponent {
+
+}
