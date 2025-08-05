6f9e24396393576bc9c0f0b4896bbeb35e65fa5a
Gabriel <gerson.santos@sgnsistemas.com.br>
2025-08-05 13:24:17 -0300

    feat: bloco de formulario

--- src/components/content/content.component.ts
+++ src/components/content/content.component.ts
@@ -3,10 +3,11 @@ import { InicioComponent } from "../inicio/inicio.component";
 import { ServicosComponent } from "../servicos/servicos.component";
 import { PagamentoComponent } from "../pagamento/pagamento.component";
 import { MotivosComponent } from "../motivos/motivos.component";
+import { OrcamentoComponent } from "../orcamento/orcamento.component";
 
 @Component({
   selector: 'app-content',
-  imports: [InicioComponent, ServicosComponent, PagamentoComponent, MotivosComponent],
+  imports: [InicioComponent, ServicosComponent, PagamentoComponent, MotivosComponent, OrcamentoComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
---
+++ src/components/orcamento/orcamento.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orcamento',
+  imports: [],
+  templateUrl: './orcamento.component.html',
+  styleUrl: './orcamento.component.css'
+})
+export class OrcamentoComponent {
+
+}
